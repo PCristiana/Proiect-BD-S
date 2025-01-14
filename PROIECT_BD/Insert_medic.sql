e02182c38670bd5b6cffef69fb97a13577c1f237
Petridean Cristiana <167132655+PCristiana@users.noreply.github.com>
2025-01-14 23:20:28 +0200

    Add files via upload

---
+++ PROIECT_BD/Insert_medic.sql
@@ -0,0 +1,9 @@
+INSERT INTO Medic (id_medic, id_angajat, specialitate, grad, cod_parafa, competente, titlu_stiintific, post_didactic, procent_servicii)
+VALUES
+(1, 1, 'Cardiologie', 'specialist', 'PARAFA001', 'Echocardiografie, Teste de efort', 'doctor', 'lector', 25.00), -- Medic specialist cardiolog
+(2, 4, 'Pediatrie', 'primar', 'PARAFA002', 'Consultatii copii, Urgente pediatrice', 'doctor', 'profesor', 30.00), -- Medic primar pediatru
+(3, 6, 'Dermatologie', 'specialist', 'PARAFA003', 'Dermatoscopie, Biopsii cutanate', 'doctorand', 'asistent', 20.00), -- Medic specialist dermatolog
+(4, 9, 'Ortopedie', 'primar', 'PARAFA004', 'Interventii artroscopice, Fracturi complexe', 'doctor', 'conferentiar', 28.00), -- Medic primar ortoped
+(5, 3, 'Neurologie', 'specialist', 'PARAFA005', 'Electroencefalografie, Tratament AVC', 'doctor', 'lector', 22.00), -- Medic specialist neurolog
+(6, 7, 'Oftalmologie', 'specialist', 'PARAFA006', 'Corectii laser, Cataracta', 'doctorand', 'asistent', 25.00); -- Medic specialist oftalmolog
+-- (7, 13, 'Chirurgie Generală', 'primar', 'PARAFA007', 'Chirurgie laparoscopică, Interventii complexe', 'doctor', 'profesor', 35.00); -- Medic primar chirurg
