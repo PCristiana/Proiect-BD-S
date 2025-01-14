77a20f76836c094128c77099c286df28e9b0de25
Petridean Cristiana <167132655+PCristiana@users.noreply.github.com>
2025-01-14 23:19:33 +0200

    Delete PROIECT_BD directory

--- PROIECT_BD/Insert_departamente.sql
+++
@@ -1,9 +0,0 @@
---actualizat ca sa corespunda cu asistenti medicali, medici, utilizatori
-use MedicalCenterBD;
-
-INSERT INTO Departamente (id_departament, nume_departament) VALUES
-(1, 'Resurse Umane'),
-(2, 'Contabilitate'),
-(3, 'Personal Medical');
-
-
